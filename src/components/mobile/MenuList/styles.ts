6d131b88934544a95e5c4b52fec20665549f7ec9
Icaro Vieira da Silva <icarovsilva1@gmail.com>
2022-08-08 14:49:48 -0300

    feat css corrections

--- src/components/mobile/MenuList/styles.ts
+++ src/components/mobile/MenuList/styles.ts
@@ -1,44 +1,50 @@
 import styled from 'styled-components';
 
 export const Container = styled.nav`
-  position: absolute;
+  display: none;
+  @media screen and (max-width: 500px) {
+    position: absolute;
 
-  width: 200px;
-  background: #fff;
+    width: 200px;
+    background: #fff;
 
-  left: 50%;
-  top: 80px;
-  transform: translateX(-50%);
+    left: 50%;
+    top: 80px;
+    transform: translateX(-50%);
 
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-
-  gap: 20px;
-
-  padding: 20px 0;
-
-  border-radius: 10px;
-
-  box-shadow: 0 0 15px 5px rgba(0, 0, 0, 0.2);
-
-  ul {
     display: flex;
+    align-items: center;
     flex-direction: column;
+
     gap: 20px;
 
-    text-align: center;
+    padding: 20px 0;
 
-    li {
+    border-radius: 10px;
+
+    box-shadow: 0 0 15px 5px rgba(0, 0, 0, 0.2);
+
+    z-index: 20;
+
+    ul {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+
+      text-align: center;
+
+      li {
+        font-weight: bold;
+        font-size: 1rem;
+      }
+    }
+
+    button {
       font-weight: bold;
       font-size: 1rem;
+      color: ${({ theme }) => theme.colors.grayishBlue};
+      background: transparent;
     }
-  }
-
-  button {
-    font-weight: bold;
-    font-size: 1rem;
-    color: ${({ theme }) => theme.colors.grayishBlue};
   }
 `;
 
